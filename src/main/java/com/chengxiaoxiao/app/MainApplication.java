9b50b63f3420b9f63a46fb59651b5f61df966e52
Cheng xiaoxiao <iquanzhan@163.com>
2020-05-01 15:21:57 +0800

    集成druid、fastjson、全局跨域、swagger

---
+++ src/main/java/com/chengxiaoxiao/app/MainApplication.java
@@ -0,0 +1,16 @@
+package com.chengxiaoxiao.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2020/5/1 1:59 下午
+ * @Description:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
+}
